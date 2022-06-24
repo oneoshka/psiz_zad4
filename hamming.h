7b6d71f29563015799272c2f788b7418ccf124cc
oneoshka <oneoshka@gmail.com>
2022-06-24 22:57:46 +0200

    Add project files.

---
+++ hamming.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2; // XOR
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
+}
